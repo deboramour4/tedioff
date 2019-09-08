5bf4e03834b566ff921ef5a710ed556963cfcc90
Débora Moura <deboramour4@gmail.com>
2019-09-07 21:06:28 -0300

    Setup MVVM+RxSwift version

---
+++ TedioffMVVM-RxSwift/Pods/Target Support Files/Pods-TedioffMVVM-RxSwiftTests/Pods-TedioffMVVM-RxSwiftTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_TedioffMVVM_RxSwiftTests : NSObject
+@end
+@implementation PodsDummy_Pods_TedioffMVVM_RxSwiftTests
+@end
---
+++ TedioffMVVM-RxSwift/Pods/Target Support Files/Pods-TedioffMVVM-RxSwiftUITests/Pods-TedioffMVVM-RxSwiftUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_TedioffMVVM_RxSwiftUITests : NSObject
+@end
+@implementation PodsDummy_Pods_TedioffMVVM_RxSwiftUITests
+@end
